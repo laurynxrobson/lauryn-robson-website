8d0326f95279f5759f08ec5fec21fbd4ae347e6b
laurynxrobson <laurynxrobson@gmail.com>
2024-10-28 21:47:20 +0200

    Changed project font

--- lauryn-robson-website/src/app/layout.tsx
+++ lauryn-robson-website/src/app/layout.tsx
@@ -3,17 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { NavLinks } from "./ui/nav-links";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: "Lauryn Robson",
   description: "This is the offical website of Lauryn Robson",
@@ -25,10 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en">
+      <body>
         <NavLinks />
         {children}
       </body>
--- lauryn-robson-website/src/app/page.tsx
+++ lauryn-robson-website/src/app/page.tsx
@@ -1,9 +1,14 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <Link className='text-3xl font-bold underline' href='/'>
-      Home
-    </Link>
+    <div className="py-56">
+      <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+        Data to enrich your online business
+      </h1>
+      <Link className="lg:hidden text-3xl font-bold underline" href="/">
+        Home
+      </Link>
+    </div>
   );
 }
