6db2f0ec98f344304a9e97e748b72069f6cf248d
laurynxrobson <laurynxrobson@gmail.com>
2024-10-28 21:35:34 +0200

    Styled the nav bar

--- lauryn-robson-website/src/app/page.tsx
+++ lauryn-robson-website/src/app/page.tsx
@@ -1,5 +1,9 @@
 import Link from 'next/link';
 
 export default function Page() {
-  return <Link href='/'>Home</Link>;
+  return (
+    <Link className='text-3xl font-bold underline' href='/'>
+      Home
+    </Link>
+  );
 }
--- lauryn-robson-website/src/app/ui/nav-links.tsx
+++ lauryn-robson-website/src/app/ui/nav-links.tsx
@@ -1,29 +1,45 @@
-'use client';
+"use client";
 
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
+const navigation = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+];
 
 export function NavLinks() {
   const pathname = usePathname();
-
   return (
-    <nav>
-      <Link className={`link ${pathname === '/' ? 'active' : ''}`} href='/'>
-        Home
-      </Link>
-
-      <Link
-        className={`link ${pathname === '/about' ? 'active' : ''}`}
-        href='/about'
-      >
-        About
-      </Link>
-      <Link
-        className={`link ${pathname === '/about' ? 'active' : ''}`}
-        href='/contact'
-      >
-        Contact
-      </Link>
-    </nav>
+    <div className="absolute inset-x-0 top-0 z-50">
+      <nav className="flex items-center justify-between">
+        <div className="flex lg:flex-1">
+          <Link className="-m-1.5 p-1.5" href="/">
+            <span className="sr-only">Logo</span>
+            <Image
+              src={"/apple-touch-icon.png"}
+              alt={"Lauryn Robson logo"}
+              width={75}
+              height={75}
+            />
+          </Link>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          <ul className="flex lg:flex-1">
+            {navigation.map((item) => (
+              <Link
+                className="text-sm font-semibold leading-6 text-gray-900 px-10"
+                href={item.href}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
+      </nav>
+    </div>
   );
 }
